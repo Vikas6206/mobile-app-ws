9afe25ba0bf1fdf630cda6bbacf923a2aa9dfeae
Vikas6206 <vikas.kr6206@gmail.com>
2024-03-01 20:52:59 +0530

    Added reading request body via annotation

--- src/main/java/com/appdelevloperblogapp/ws/ui/controller/UserController.java
+++ src/main/java/com/appdelevloperblogapp/ws/ui/controller/UserController.java
@@ -1,29 +1,71 @@
 package com.appdelevloperblogapp.ws.ui.controller;
 
+import com.appdelevloperblogapp.ws.ui.model.request.UserDetailsRequestModel;
+import com.appdelevloperblogapp.ws.ui.model.response.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users") //http://localhist:8080/users
 public class UserController {
 
-    @GetMapping
-    public String getUser(){
-        return "get user was called";
+    /**
+     * MediaType is used to support json as well as xml value, update pom.xml-> jackson dependency
+     * @param userId
+     * @return
+     */
+    @GetMapping(path = "/{userId}",
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+
+        if (userId.equals("-1")) {
+            return ResponseEntity.badRequest().build();
+        }
+        UserRest userRest = new UserRest();
+        userRest.setEmail("ak@gmail.com");
+        userRest.setFirstName("Vikas");
+        userRest.setLastName("Kumar");
+        userRest.setUserId(userId);
+        return new ResponseEntity<UserRest>(userRest,HttpStatus.OK);
     }
 
+    /**
+     *
+     * @param page making this as optional or providing default value
+     * @param limit
+     * @return
+     */
+    @GetMapping()
+    public String getUsers(@RequestParam(value = "page",defaultValue = "1") int page,
+                           @RequestParam(value = "limit",defaultValue = "25") int limit,
+                           @RequestParam(value = "sort", defaultValue = "ascending",required = false) String sort){
+        return "get user was called with page = " + page + " and limit = " + limit +" sort val "+sort;
+    }
 
-    @PostMapping
-    public String createUser(){
-        return "create user was called";
+    /**
+     * read json/xml request body payload
+     * @return
+     */
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+                 produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+        UserRest userRest = new UserRest();
+        userRest.setEmail(userDetails.getEmail());
+        userRest.setFirstName(userDetails.getFirstName());
+        userRest.setLastName(userDetails.getLastName());
+        userRest.setUserId(userDetails.getPassword());
+        return new ResponseEntity<UserRest>(userRest,HttpStatus.OK);
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "update user was called";
     }
 
     @DeleteMapping
-    public  String deleteUser(){
+    public String deleteUser() {
         return "delete user was called";
     }
 }
