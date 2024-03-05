904b44c660ea044db8147ac1c0ad130e7f9ce891
Vikas6206 <vikas.kr6206@gmail.com>
2024-03-05 20:06:25 +0530

    added the delete logic

--- src/main/java/com/appdelevloperblogapp/ws/ui/controller/UserController.java
+++ src/main/java/com/appdelevloperblogapp/ws/ui/controller/UserController.java
@@ -82,8 +82,9 @@ public class UserController {
         return storedUserDetails;
     }
 
-    @DeleteMapping
-    public String deleteUser() {
-        return "delete user was called";
+    @DeleteMapping(path = "/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+        users.remove(userId);
+        return ResponseEntity.noContent().build();
     }
 }
